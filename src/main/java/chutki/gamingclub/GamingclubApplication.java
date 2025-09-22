db0c7e1c0195ff890f0ead3cb33878836a0f7d5e
bharathi322 <bharathi.gk499@gmail.com>
2025-09-22 14:49:16 +0530

    Initial commit

---
+++ src/main/java/chutki/gamingclub/GamingclubApplication.java
@@ -0,0 +1,13 @@
+package chutki.gamingclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingclubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chutki/gamingclub/GamingclubApplicationTests.java
@@ -0,0 +1,13 @@
+package chutki.gamingclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
